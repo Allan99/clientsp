19e69b1dd65ebc17efde0d9083d523f16eb16e21
Allan <allanpereira773@gmail.com>
2026-04-12 18:06:26 -0300

    Setting up project, creating Client entity and tb_client table

---
+++ src/main/java/com/devsuperior/clientsp/ClientspApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.clientsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/clientsp/ClientspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.clientsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
