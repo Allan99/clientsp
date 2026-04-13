74d8cb1b91e58d72e7663365635d9c46113b3fa4
Allan <allanpereira773@gmail.com>
2026-04-13 13:58:53 -0300

    Adding paged search for clients and findAll method

--- src/main/java/com/devsuperior/clientsp/services/ClientService.java
+++ src/main/java/com/devsuperior/clientsp/services/ClientService.java
@@ -5,8 +5,12 @@ import com.devsuperior.clientsp.entities.Client;
 import com.devsuperior.clientsp.repositories.ClientRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ClientService {
@@ -21,5 +25,11 @@ public class ClientService {
     public ClientDTO findById(Long id){
         Client client = repository.findById(id).get();
         return modelMapper.map(client, ClientDTO.class);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable){
+        Page<Client> clients = repository.findAll(pageable);
+        return clients.map(x -> modelMapper.map(x, ClientDTO.class));
     }
 }
